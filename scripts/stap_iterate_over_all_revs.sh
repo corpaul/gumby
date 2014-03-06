63983b93155cff14862a7718332029d785ebe416
Cor-Paul Bezemer <c.bezemer@tudelft.nl>
2014-03-06 14:51:47 +0100

    git setup

--- scripts/stap_iterate_over_all_revs.sh
+++ scripts/stap_iterate_over_all_revs.sh
@@ -95,8 +95,7 @@ for REV in $(git log $GIT_LOG_CMD | grep ^"commit " | cut -f2 -d" "); do
     export REVISION=$REV
     ITERATION=0
 
-	./configure
-	CFLAGS=-g make
+	make j`cat /proc/cpuinfo | grep processor | wc -l`
 
 	while [ $ITERATION -lt $STAP_RUN_ITERATIONS ]; do
         let ITERATION=1+$ITERATION
