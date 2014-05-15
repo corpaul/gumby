b6e91fd9ffaf1205e479e4b3458385c4abae3497
CP <c.bezemer@tudelft.nl>
2014-05-15 14:20:57 +0200

    add netem stuff to concurrent

--- experiments/libswift/concurrent_container_cmd.sh
+++ experiments/libswift/concurrent_container_cmd.sh
@@ -1,15 +1,49 @@
 #!/bin/bash -xe
 # %*% Starts a lightweight web server in a container to seed a file of arbitrary size.
 
-EXPECTED_ARGS=1
+EXPECTED_ARGS=5
 if [ $# -ne $EXPECTED_ARGS ]
 then
-	echo "Usage: `basename $0` output_dir "
+	echo "Usage: `basename $0` output_dir CONCURRENT_DELAY CONCURRENT_PACKET_LOSS CONCURRENT_RATE CONCURRENT_RATE_UL"
 	exit 65
 fi
 
 # TODO use getopts
 OUTPUT_DIR="$1"
+NETEM_DELAY="$2"
+NETEM_PACKET_LOSS="$3"
+NETEM_RATE_DL="$4"
+NETEM_RATE_UL="$5"
+
+
+
+# fix formatting for random variation
+# @CONF_OPTION SEEDER_DELAY: Netem delay for the seeder.
+NETEM_DELAY=${NETEM_DELAY/'_'/' '}
+# @CONF_OPTION SEEDER_RATE: Download rate limit for the seeder. Configure the rate as rate_burst, so e.g. seeder_rate="1mbit_100k"
+IFS='_' read -ra RATE_DL <<< "$NETEM_RATE_DL"
+RATE_DL=${RATE_DL[0]}
+BURST_DL=${RATE_DL[1]}
+# @CONF_OPTION SEEDER_RATE_UL: Upload rate limit for the seeder. Configure the rate as rate_burst, so e.g. seeder_rate_ul="1mbit_100k"
+IFS='_' read -ra RATE_UL <<< "$NETEM_RATE_UL"
+RATE_UL=${RATE_UL[0]}
+BURST_UL=${RATE_UL[1]}
+
+# ingress traffic
+tc qdisc add dev eth0 handle ffff: ingress
+tc filter add dev eth0 parent ffff: protocol ip prio 50 \
+   u32 match ip src 0.0.0.0/0 police rate $RATE_DL \
+   burst $BURST_DL drop flowid :1
+
+# egress traffic
+tc qdisc add dev eth0 root handle 1: netem delay $NETEM_DELAY loss $NETEM_PACKET_LOSS
+
+# add netem stuff
+tc qdisc add dev eth0 parent 1: tbf rate $RATE_UL limit $BURST_UL burst $BURST_UL
+
+# !--------------------
+
+tc qdisc show
 
 # start server
 lighttpd -D -f $OUTPUT_DIR/lighttpd/lighttpd.conf &
