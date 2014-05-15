1c6714bb567b037a97625c7fb74d8e2de96c7f44
CP <c.bezemer@tudelft.nl>
2014-05-15 11:11:47 +0200

    use concurrent download

--- experiments/libswift/run_experiment.sh
+++ experiments/libswift/run_experiment.sh
@@ -244,8 +244,11 @@ for (( i = 0 ; i < $NO_OF_LEECHERS; i++ ))
 do
 	LEECHER_ID_TMP=$(($LEECHER_ID+$i))
 	rm -f $OUTPUT_DIR/dst/$LEECHER_ID_TMP/$HASH*
-	rm -f $OUTPUT_DIR/dst/$LEECHER_ID_TMP/concurrent/*
-	rmdir -p $OUTPUT_DIR/dst/$LEECHER_ID_TMP/concurrent
+	if $CONCURRENT_DOWNLOAD;
+	then
+		rm -f $OUTPUT_DIR/dst/$LEECHER_ID_TMP/concurrent/*
+		rmdir -p $OUTPUT_DIR/dst/$LEECHER_ID_TMP/concurrent
+	fi
 done
 
 
