09ffea0e5dadc95b9cfe3efe2c1321673e05bcf2
Cor-Paul Bezemer <c.bezemer@tudelft.nl>
2014-05-15 12:31:27 +0200

    Update leecher_container_cmd.sh

--- experiments/libswift/leecher_container_cmd.sh
+++ experiments/libswift/leecher_container_cmd.sh
@@ -87,7 +87,7 @@ else
 	then
 		su $USERNAME -c "mkdir -p $LOGS_DIR/dst/$LEECHER_ID/concurrent"
 		# @CONF_OPTION CONCURRENT_START_TIME: Time to wait before downloading the concurrent file
-		su $USERNAME -c "sleep $CONCURRENT_START_TIME && wget http://$CONCURRENT_IP:3000/dl.zip -o $LOGS_DIR/dst/$LEECHER_ID/concurrent/dl.zip &"
+		su $USERNAME -c "wget http://$CONCURRENT_IP:3000/dl.zip -o $LOGS_DIR/dst/$LEECHER_ID/concurrent/dl.zip"
 	fi
 fi
 
