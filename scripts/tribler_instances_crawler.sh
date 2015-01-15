c93cb3e5a593cc0b472040539217998d0d62960f
CP <c.bezemer@tudelft.nl>
2015-01-15 13:50:27 +0100

    start crawler as well

--- scripts/tribler_instances_crawler.sh
+++ scripts/tribler_instances_crawler.sh
@@ -4,8 +4,13 @@
 INSTANCE=0
 while [ $INSTANCE -lt $TRIBLER_INSTANCES ]; do
 	let INSTANCE=1+$INSTANCE
-	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' -m $OUTPUT_DIR -o $OUTPUT_DIR &
+	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' -m $OUTPUT_DIR/$INSTANCE -o $OUTPUT_DIR/$INSTANCE &
 done
+
+# start crawler
+cd tribler/Tribler			
+process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'twistd barter_crawler --statedir=$OUTPUT_DIR/crawler' -m $OUTPUT_DIR/crawler -o $OUTPUT_DIR/crawler &	
+	
 	
 wait
 
