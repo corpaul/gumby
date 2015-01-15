2947c8660932bc882336f7e78b4bc52924228bd5
CP <c.bezemer@tudelft.nl>
2015-01-15 13:23:56 +0100

    move start of tribler instances to script

---
+++ scripts/tribler_instances_crawler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+
+INSTANCE=0
+while [ $INSTANCE -lt $TRIBLER_INSTANCES ]; do
+	let INSTANCE=1+$INSTANCE
+	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' &
+done
+	
+wait
+
