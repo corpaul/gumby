e91d141f2db9b991ed63d92a66f2d931dac47dc5
CP <c.bezemer@tudelft.nl>
2015-01-15 13:30:21 +0100

    fix output dir

--- scripts/tribler_instances_crawler.sh
+++ scripts/tribler_instances_crawler.sh
@@ -4,7 +4,7 @@
 INSTANCE=0
 while [ $INSTANCE -lt $TRIBLER_INSTANCES ]; do
 	let INSTANCE=1+$INSTANCE
-	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' &
+	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' -m $OUTPUT_DIR -o $OUTPUT_DIR &
 done
 	
 wait
