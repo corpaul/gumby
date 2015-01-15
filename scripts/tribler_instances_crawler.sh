01c77b2231337d6e6efceb148b15c646064a0109
CP <c.bezemer@tudelft.nl>
2015-01-15 14:10:51 +0100

    fix path

--- scripts/tribler_instances_crawler.sh
+++ scripts/tribler_instances_crawler.sh
@@ -7,6 +7,8 @@ while [ $INSTANCE -lt $TRIBLER_INSTANCES ]; do
 	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' -m $OUTPUT_DIR/$INSTANCE -o $OUTPUT_DIR/$INSTANCE &
 done
 
+export PYTHONPATH=$PYTHONPATH:$PWD/tribler
+
 # start crawler
 cd tribler/Tribler			
 process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'twistd barter_crawler --statedir=$OUTPUT_DIR/crawler' -m $OUTPUT_DIR/crawler -o $OUTPUT_DIR/crawler &	
