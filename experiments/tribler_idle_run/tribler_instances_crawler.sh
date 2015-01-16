7935c326b83d63cd680063165d56d67d13544e06
Cor-Paul Bezemer <c.bezemer@tudelft.nl>
2015-01-16 09:38:25 +0100

    change to use statedir per instance

--- experiments/tribler_idle_run/tribler_instances_crawler.sh
+++ experiments/tribler_idle_run/tribler_instances_crawler.sh
@@ -2,8 +2,9 @@
 
 
 INSTANCE=0
-while [ $INSTANCE -lt $TRIBLER_INSTANCES ]; do
+while [ $INSTANCE -lt $TRIBLER_INSTANCE+1 ]; do
 	let INSTANCE=1+$INSTANCE
+	export INSTANCE_ID=$INSTANCE
 	process_guard.py -t $PROCESS_GUARD_TIMEOUT -c 'wrap_in_vnc.sh gumby/experiments/tribler_idle_run/tribler_idle_run.py' -m $OUTPUT_DIR/$INSTANCE -o $OUTPUT_DIR/$INSTANCE &
 done
 
@@ -11,9 +12,9 @@ export PYTHONPATH=$PYTHONPATH:$PWD/tribler
 
 mkdir -p $OUTPUT_DIR/crawler/sqlite
 # start crawler
-cd tribler/Tribler			
+cd tribler/Tribler
 twistd -n -l- bartercast_crawler --statedir=$OUTPUT_DIR/crawler
-	
-	
+
+
 wait
 
